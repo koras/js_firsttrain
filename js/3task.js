570f70dd287035e0ad3b405a622be1f0a7b88d72
Константин <kos6476@yandex.ru>
2022-01-26 12:55:02 +0300

    Update 3task.js

--- js/3task.js
+++ js/3task.js
@@ -1,18 +1,4 @@
-/*for (i = 0; i < 8; i++)
-{
-    for (j = 0; j < 8; j++)
-    {
-        if (i % 2 == 0 && j % 2 == 0)
-            console.log('#');
-        if (i % 2 == 0 && j % 2 != 0)
-            console.log(' ');
-        if (i % 2 != 0 && j % 2 == 0)
-            console.log(' ');
-        if (i % 2 != 0 && j % 2 != 0)
-            console.log("#");
-    }
-    console.log('\n')
-} */
+ 
 let board = "";
 
 for (var i = 0; i < 8; i++) {
