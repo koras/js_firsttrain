1472711357bdd63c4fc240ebe4386816cff443e3
Константин <kos6476@yandex.ru>
2022-01-26 12:52:58 +0300

    Update 2task.js
    Отлично

--- js/2task.js
+++ js/2task.js
@@ -1,10 +1,10 @@
 for (thislo = 1; thislo <= 100; thislo++){
-    /*
+    
     if (thislo % 3 == 0)
         console.log('Fizz');
     else if (thislo % 5 == 0)
         console.log('Buzz');
-    */
+    
     if (thislo % 3 == 0 && thislo % 5 == 0)
         console.log('FizzBuzz');
     else
